070b8c546663047602550962172c4ccba16b7e45
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2023-03-30 14:46:06 +0300

    completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   
+  # in this method, we're setting values on the session
+  # hash, and the cookies hash, and serializing them in 
+  # the response so we can view their values in the browser.
+
+  # ||= syntax is a shorthand way to assign a value, if its current value is nil or false. 
+  # So, if session[:session_hello] hasn't been assigned a value, it'll be assigned a value of "World".
+  # Otherwise, it won't get assigned a new value
   def index
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
